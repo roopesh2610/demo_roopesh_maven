14785868c49932e7564ffed3a61b72b43afa70ba
roopesh <roopesh2610@gmail.com>
2016-12-21 16:08:37 -0600

    new maven project in git

---
+++ demo_roopesh_maven/src/main/java/company_name/com/App.java
@@ -0,0 +1,13 @@
+package company_name.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
